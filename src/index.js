19a40b88639af35c3a6b6f1b8fd6275ce6478b93
BK <DilaBK@macBook>
2022-10-16 21:53:35 +0700

    Tic Tac Toe Game Done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,142 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button 
+//         className="square" 
+//         onClick={() => {
+//           this.props.onnClick()
+//         }}>
+//         {/* TODO */}{this.props.nilai}
+//       </button>
+//     );
+//   }
+// }
+function Square(props) {
+  return (  
+    <button 
+      className="square" 
+      onClick={props.onnClick}
+    >
+      {/* TODO */}{props.nilai}
+    </button>
+  )
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  async handleClick(val) {
+    const squares = this.state.squares.slice();
+    if (squares[val] || findTheWinner(this.state.squares)) {
+      return;
+    }
+    squares[val] = this.state.xIsNext ? 'X' : 'O';
+    console.log('new input : ' + squares);
+    await this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+    console.log('our board now : ' + this.state.squares);
+  }
+
+  fungs(i) {
+    return (
+      <Square 
+        nilai={this.state.squares[i]}
+        onnClick={() => this.handleClick(i)}
+      />
+    )
+  }
+
+  render() {
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const win = findTheWinner(this.state.squares);
+    let status;
+    if (win) {
+      status = 'The Winner is : ' + win;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.fungs(0)}
+          {this.fungs(1)}
+          {this.fungs(2)}
+        </div>
+        <div className="board-row">
+          {this.fungs(3)}
+          {this.fungs(4)}
+          {this.fungs(5)}
+        </div>
+        <div className="board-row">
+          {this.fungs(6)}
+          {this.fungs(7)}
+          {this.fungs(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function findTheWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[b] === squares[a] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+
+
