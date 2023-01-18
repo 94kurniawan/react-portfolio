0f74e604a0d1b3623085166396d3e51e2b1203bd
BK-Geekseat <kurniawantkj2012@gmail.com>
2023-01-18 16:04:26 +0700

    Hooks Implement (useState and useEffect)

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./../components/Header";
 import Intro from "./../components/Intro";
 import About from "./../components/About";
@@ -10,16 +10,36 @@ import Footer from "./../components/Footer";
 let title = "Senior Frontend Engineer / Developer & UI/UX Designer"
 
 function Main() {
-    const [getName, setName] = useState({})
+    const [getName, setName] = useState({name:"", address:"Jalan"})
+    const [group, setGroup] = useState("G");
     const changeName = () => {
-        setName({name:'changed name'})
+        // setName({name:'BB'})
+        setName({name:'BB',address: 'Jalan BB'})
+        document.title = 'test'
     }
+    const changeGroup = () => {
+        setGroup("New Group")
+    }
+    const customAlert = () => {
+        console.log('custom alert call');
+    }
+
+    useEffect(() => {
+        // alert('group Effect')
+        customAlert()
+    },[group])
+
+    // useEffect(() => {
+    //     alert('Name effect')
+    //     document.title = getName.address
+    // },[getName])
     return (
         <div className="layout layout__main">
             <div className="bg-yellow-400 p-3 text-center">
                 <button onClick={() => changeName()} className="bg-gray-100 p-2">
                     {getName.name}
                 </button>
+                <button onClick={() => changeGroup()} className="bg-gray-100 p-2 mx-2">{group}</button>
             </div>
             <div className="layout__main--header"><Header /></div>
             <div className="layout__main--content">
